9a2b60d26a603d38ca94f95f21849e0c85438386
SuleimenZ <62009948+SuleimenZ@users.noreply.github.com>
2020-05-23 15:41:45 +0200

    Recoded so it uses classes instead of functions

--- Project01/Program.cs
+++ Project01/Program.cs
@@ -10,101 +10,10 @@ namespace Project01
 {
     class Program
     {
-        public static int[] GenerateRandomArray(int size)
-        {
-            var rnd = new Random();
-            var array = new int[size];
-
-            for (int i = 0; i < size; i++)
-            {
-                array[i] = rnd.Next(int.MinValue, int.MaxValue);
-            }
-
-            return array;
-        }
-
-        public static int[] GenerateSortedRandomArray(int size)
-        {
-            var sortedArray = new int[size];
-            var array = new int[size];
-
-            sortedArray = GenerateRandomArray(size);
-            Array.Sort(sortedArray);
-
-            return sortedArray;
-        }
-
-        static bool SimpleLinearSearch(int[] array, int searchedValue)
-        {
-            bool answer = false;
-            for (int i = 0; i < array.Length; i++)
-            {
-                if ( searchedValue == array[i])
-                {
-                    answer = true;
-                }
-            }
-            return answer;
-        }
-
-        static bool ImprovedLinearSearch(int[] array, int searchedValue)
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (searchedValue == array[i]) 
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        static bool ImprovedLinearSearchWithSentinel(int[] array, int searchedValue)
-        {
-            int i = 0;
-            var Last = array.Last();
-            array[array.Length-1] = searchedValue;
-            while (array[i] != searchedValue)
-            {
-                i++;
-            }
-            array[array.Length - 1] = Last;
-            if (i < array.Length - 1 || (searchedValue == Last))
-            {
-                return true;
-            }
-            return false;
-        }
-
-        static bool BinarySearch(int[] array, int searchedValue)
-        {
-            int floor = 0;
-            int ceiling = array.Length - 1;
-
-            while(floor < ceiling)
-            {
-                int middle = ((ceiling + floor) / 2);
-                if (middle == searchedValue)
-                {
-                    return true;
-                }
-                else if (middle < searchedValue)
-                {
-                    floor = middle + 1;
-                }
-                else
-                {
-                    ceiling = middle - 1;
-                }
-            }
-            return false;
-        }
-
-
         static void Main(string[] args)
         {
             int searchedValue = 0;
-            int size = 10000;
+            int size = 0;
             int[] array;
             char userInput;
             bool isInt = false;
@@ -137,7 +46,7 @@ namespace Project01
                     //Generates 10 results
                     for (int i = 0; i < 11; i++) // ignore first result(i=0) because it takes much more time to run code first time so we get more precise results
                     {
-                        array = GenerateRandomArray(size);
+                        array = RandomArray.Generate(size);
 
                         var firstValue = array.First();             //first value in the array
                         var randomValue = array[rnd.Next(0, size)]; //random value in the array
@@ -152,19 +61,19 @@ namespace Project01
                         Console.WriteLine(i);
 
                         stopwatch.Start();
-                        SimpleLinearSearch(array, searchedValue);
+                        RandomArray.SimpleLinearSearch(array, searchedValue);
                         stopwatch.Stop();
                         Console.WriteLine($"Simple:   {stopwatch.Elapsed.TotalMilliseconds} ms");
                         stopwatch.Reset();
 
                         stopwatch.Start();
-                        ImprovedLinearSearch(array, searchedValue);
+                        RandomArray.ImprovedLinearSearch(array, searchedValue);
                         stopwatch.Stop();
                         Console.WriteLine($"Improved: {stopwatch.Elapsed.TotalMilliseconds} ms");
                         stopwatch.Reset();
 
                         stopwatch.Start();
-                        ImprovedLinearSearchWithSentinel(array, searchedValue);
+                        RandomArray.ImprovedLinearSearchWithSentinel(array, searchedValue);
                         stopwatch.Stop();
                         Console.WriteLine($"Sentinel: {stopwatch.Elapsed.TotalMilliseconds} ms");
                         stopwatch.Reset();
@@ -176,12 +85,12 @@ namespace Project01
                 case '4':
                     for (int i = 0; i < 11; i++) // ignore first result(i=0) because it takes much more time to run code first time so we get more precise results
                     {
-                        array = GenerateSortedRandomArray(size);
+                        array = RandomArray.GenerateSorted(size);
 
                         Console.WriteLine(i);
 
                         stopwatch.Start();
-                        BinarySearch(array, array.Last());
+                        RandomArray.BinarySearch(array, array.Last());
                         stopwatch.Stop();
                         Console.WriteLine($"Binary: {stopwatch.Elapsed.TotalMilliseconds}");
                         stopwatch.Reset();
---
+++ Project01/RandomArray.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project01
+{
+    class RandomArray
+    {
+
+        public static int[] Generate(int size)
+        {
+            var rnd = new Random();
+            var array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rnd.Next(int.MinValue, int.MaxValue);
+            }
+
+            return array;
+        }
+
+        public static int[] GenerateSorted(int size)
+        {
+            var array = Generate(size);
+            Array.Sort(array);
+
+            return array;
+        }
+
+        public static bool SimpleLinearSearch(int[] array, int searchedValue)
+        {
+            bool answer = false;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (searchedValue == array[i])
+                {
+                    answer = true;
+                }
+            }
+            return answer;
+        }
+
+        public static bool ImprovedLinearSearch(int[] array, int searchedValue)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (searchedValue == array[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool ImprovedLinearSearchWithSentinel(int[] array, int searchedValue)
+        {
+            int i = 0;
+            var Last = array.Last();
+            array[array.Length - 1] = searchedValue;
+            while (array[i] != searchedValue)
+            {
+                i++;
+            }
+            array[array.Length - 1] = Last;
+            if (i < array.Length - 1 || (searchedValue == Last))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool BinarySearch(int[] array, int searchedValue)
+        {
+            int floor = 0;
+            int ceiling = array.Length - 1;
+
+            while (floor < ceiling)
+            {
+                int middle = ((ceiling + floor) / 2);
+                if (middle == searchedValue)
+                {
+                    return true;
+                }
+                else if (middle < searchedValue)
+                {
+                    floor = middle + 1;
+                }
+                else
+                {
+                    ceiling = middle - 1;
+                }
+            }
+            return false;
+        }
+    }
+}
